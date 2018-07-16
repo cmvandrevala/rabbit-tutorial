3dbd0852183351297a811b9d7c938cab5966b237
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2018-07-16 14:46:45 -0500

    Allow user to enter input in terminal

--- send.rb
+++ send.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 require 'bunny'
 
+user_input = ARGV.join(" ")
+
 connection = Bunny.new(automatically_recover: false)
 connection.start
 
 channel = connection.create_channel
 queue = channel.queue('hello')
 
-channel.default_exchange.publish('Hello World!', routing_key: queue.name)
-puts " [x] Sent 'Hello World!'"
+channel.default_exchange.publish(user_input, routing_key: queue.name)
+puts " [x] Sent '#{user_input}'"
 
 connection.close
